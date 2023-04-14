65cfdaa122438da3778316132713c0239fc847f2
danishprasla <danishprasla@hotmail.com>
2023-04-14 08:46:15 -0500

    Login feature added

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,8 +1,58 @@
-const express = require('express')
+const express = require('express');
+const { Op } = require('sequelize');
+const bcrypt = require('bcryptjs');
+
+const { setTokenCookie, restoreUser } = require('../../utils/auth'); //importing authorization fx
+const { User } = require('../../db/models');
+
 const router = express.Router();
 
+router.post('/', async (req, res, next) => {
+  const { credential, password } = req.body
+  const user = await User.unscoped().findOne({ //removing the default scope
+    where: {
+      [Op.or]: { //returning one if the username OR the email matches the credential
+        username: credential,
+        email: credential
+      }
+    }
+  })
+  // console.log('hashed pw', user.hashedPassword);
+
+  //if user is null aka no user found OR the hashed version off the input pass word doesnt match the stored HashedPassword (retrieved from the user obj)
+  if (!user || !bcrypt.compareSync(password, user.hashedPassword.toString())) {
+    const err = new Error('Login failed');
+    err.status = 401;
+    err.title = 'Login failed';
+    err.errors = {
+      credential: 'The provided credentials were invalid.'
+    }
+   
+    return next(err)
+  }
+  const safeUser = {
+    id: user.id,
+    email: user.email,
+    username: user.username
+  }
+
+  await setTokenCookie(res, safeUser) //setting the token when user logs in
+
+  return res.json({
+    user: safeUser
+  })
+})
 
 
+//code to test above route
+// fetch('/api/session', {
+//   method: 'POST',
+//   headers: {
+//     "Content-Type": "application/json",
+//     "XSRF-TOKEN": "2raIwm0K-KklVxI5Pv6JMwc0VwVTd--qSiWg"
+//   },
+//   body: JSON.stringify({ credential: 'demo@user.io', password: 'Hello World!' })
+// }).then(res => res.json()).then(data => console.log(data));
 
 
 
