34ed648c7f701d25fb29c75e2b6982e124a68322
danishprasla <danishprasla@hotmail.com>
2023-04-19 18:07:24 -0500

    find bookings curr user working on test serv

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -35,9 +35,42 @@ router.get('/current', requireAuth, async (req, res, next) => {
     err.status = 404
     return next(err)
   }
+  for (let booking of bookings) {
+    let spotId = booking.spotId
+    // console.log(spotId)
+    const previewImage = await SpotImage.findOne({
+      where: {
+        spotId: spotId,
+        preview: true
+      }
+    })
+    //if there is no preview image
+    if (!previewImage) {
+      booking.dataValues.Spot.dataValues.previewImage = 'This spot does not have a preview image'
+    } else { //if there is a preview image
+      booking.dataValues.Spot.dataValues.previewImage = previewImage.url
+    }
 
-  
-  res.json(bookings)
+    //start date split
+    let startDateRaw = booking.startDate
+    let endDateRaw = booking.endDate
+    // let startDate = startDateRaw.split('T')
+    let startDate = startDateRaw.toJSON().split('T')[0]
+    let endDate = endDateRaw.toJSON().split('T')[0]
+
+    booking.dataValues.startDate = startDate
+    booking.dataValues.endDate = endDate
+
+  }
+
+  res.status(200).json({Bookings: bookings})
 })
+
+
+// const date = new Date();
+// const options = { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute:'2-digit', second:'2-digit' };
+// const formattedDate = date.toLocaleString('en-US', options).replace(',', '');
+
+// console.log(formattedDate);
 
 module.exports = router;
