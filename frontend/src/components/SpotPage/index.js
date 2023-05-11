7a193e4415f2cd0db29ee76c01ea48dbab816ecf
danishprasla <danishprasla@hotmail.com>
2023-05-11 12:38:56 -0500

    s/o jefferson for the review issue

--- frontend/src/components/SpotPage/index.js
+++ frontend/src/components/SpotPage/index.js
@@ -33,11 +33,15 @@ const SpotPageIndex = () => {
   // console.log(reviewsObj)
 
   useEffect(() => {
-    dispatch(loadOneSpotThunk(spotId))
+    // dispatch(loadOneSpotThunk(spotId))
     // console.log('spot id in useEffect ->', spotId)
     dispatch(loadSpotReviewsThunk(spotId))
 
   }, [dispatch])
+
+  useEffect(() => {
+    dispatch(loadOneSpotThunk(spotId));
+  }, [dispatch, spotId, reviewsObj]);
 
   const spotsObj = useSelector(state => state.spots)
   // console.log('test', spotsObj[spotId])
@@ -79,14 +83,14 @@ const SpotPageIndex = () => {
       <div className="spot-images">
         {previewImage && (
           <div className="spot-preview">
-            <img src={previewImage.url} />
+            <img className="spot-preview-image" src={previewImage.url} />
           </div>
         )}
         {imageUrls.length > 0 &&
 
-          <div className="other-images">
+          <div className="other-images-container">
             {imageUrls.map(url => (
-              <img key={url} src={url} />
+              <img className="other-images" key={url} src={url} />
             ))}
           </div>
         }
@@ -112,24 +116,23 @@ const SpotPageIndex = () => {
                 </span>
               </div>
 
-              <div className="review-details-booking">
-                <div className="booking-review-section">
+              <div className="booking-review-section">
 
-                  <i className="fas fa-star"></i>
-                  {spot.numReviews === 0 ? (
-                    spot.avgStarRating
-                  ) : `${spot.avgStarRating} \u00b7 ${spot.numReviews} ${spot.numReviews === 1 ? "review" : "reviews"}`}
-                </div>
-                {(userId !== ownerId && user) && (
-                  <div className="booking-button">
-                    <button
-                      className="reserve-button"
-                      onClick={() => alert('Feature Coming Soon...')}
-                    > Reserve </button>
-                  </div>
-                )}
+                <i className="fas fa-star"></i>
+                {spot.numReviews === 0 ? (
+                  spot.avgStarRating
+                ) : `${spot.avgStarRating} \u00b7 ${spot.numReviews} ${spot.numReviews === 1 ? "review" : "reviews"}`}
               </div>
             </div>
+
+            {(userId !== ownerId && user) && (
+              <div className="booking-button">
+                <button
+                  className="reserve-button"
+                  onClick={() => alert('Feature Coming Soon...')}
+                > Reserve </button>
+              </div>
+            )}
           </div>
         </div>
       }
